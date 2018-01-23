8b47df125218df71a3ab07a96491152c65242fe7
Michele Gerarduzzi <michelegera@users.noreply.github.com>
2018-01-23 22:48:23 +0100

    Improve test coverage

--- spec/lib/administrate/field/simple_markdown_spec.rb
+++ spec/lib/administrate/field/simple_markdown_spec.rb
@@ -7,6 +7,26 @@ describe Administrate::Field::SimpleMarkdown do
   let(:text) { 'foo is the new bar' }
   let(:html) { '<p><strong>foo</strong> is the new <em>bar</em></p>' }
 
+  describe '#data' do
+    let(:output) { subject.data }
+
+    context 'with nil' do
+      let(:data) { nil }
+
+      it 'returns an empty string' do
+        expect(output).to eq ''
+      end
+    end
+
+    context 'with data' do
+      let(:data) { text }
+
+      it 'returns the data' do
+        expect(output).to eq text
+      end
+    end
+  end
+
   describe '#to_s' do
     let(:output) { subject.to_s }
 
@@ -31,6 +51,34 @@ describe Administrate::Field::SimpleMarkdown do
 
       it 'strips out the formatting' do
         expect(output).to eq "#{text}\n"
+      end
+    end
+  end
+
+  describe '#to_html' do
+    let(:output) { subject.to_html }
+
+    context 'with nil' do
+      let(:data) { nil }
+
+      it 'returns an empty string' do
+        expect(output).to eq ''
+      end
+    end
+
+    context 'with a string' do
+      let(:data) { text }
+
+      it 'wraps it in a paragraph' do
+        expect(output).to eq "<p>#{text}</p>\n"
+      end
+    end
+
+    context 'with Markdown' do
+      let(:data) { md }
+
+      it 'converts it to HTML' do
+        expect(output).to eq "#{html}\n"
       end
     end
   end
